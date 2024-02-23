a8aeaa8f754c7ee6c596255f0907ebd1d3e4e2cf
Alan Drescher <alandrescher10@gmail.com>
2024-02-23 08:37:14 +0100

    cambio

--- src/zombicide/Espada.java
+++ src/zombicide/Espada.java
@@ -2,7 +2,7 @@ package zombicide;
 
 public class Espada extends Arma {
 
-	protected Espada() {
+	protected Espada(String toString, int damage, int range, int hit) {
 		super.Arma("Espada corta", 1, 1, 4);
 		super.toString("Mata gratis a 2 zombies aleatorios.");
 	}
