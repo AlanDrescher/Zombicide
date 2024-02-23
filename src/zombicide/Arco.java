ea3d4242ebe90a58d4e4d1d400ab118995c151f0
Alan Drescher <alandrescher10@gmail.com>
2024-02-23 08:40:19 +0100

    arreglado

--- src/zombicide/Arco.java
+++ src/zombicide/Arco.java
@@ -3,7 +3,7 @@ package zombicide;
 public class Arco extends Arma {
 
 	protected Arco(String toString, int damage, int range, int hit) {
-		super.Arma(toString, damage, range, hit);
+		super(toString, damage, range, hit);
 		super.toString("Mata gratis a 1 corredor.");
 	}
 }
--- src/zombicide/Espada.java
+++ src/zombicide/Espada.java
@@ -3,7 +3,7 @@ package zombicide;
 public class Espada extends Arma {
 
 	protected Espada(String toString, int damage, int range, int hit) {
-		super.Arma("Espada corta", 1, 1, 4);
+		super("Espada corta", 1, 1, 4);
 		super.toString("Mata gratis a 2 zombies aleatorios.");
 	}
 }
