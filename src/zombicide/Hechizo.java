c9e31258e476973d0768ad76dbe231e603809631
Alan Drescher <alandrescher10@gmail.com>
2024-02-23 08:10:20 +0100

    habilidades

--- src/zombicide/Hechizo.java
+++ src/zombicide/Hechizo.java
@@ -4,11 +4,7 @@ public class Hechizo extends Arma{
 	
 	protected Hechizo(String name, int damage, int range, int hit) {
 		super.Arma(name, damage, range, hit);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public String toString() {
-		return "";
+		super.toString("Mata gratis a 2 caminantes.");
 	}
 	
 }
