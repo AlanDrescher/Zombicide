8064f944f4455dfcdb95ed922bc5a1c7a38988ec
Alan Drescher <alandrescher10@gmail.com>
2024-03-01 11:01:06 +0100

    rey

---
+++ src/zombicide/ZombieRey.java
@@ -0,0 +1,11 @@
+package zombicide;
+
+public class ZombieRey extends Zombie{
+	
+		protected ZombieRey() {
+		super("Zombie", 7, 7, true);
+		super.setMovement(1);
+		super.setDamage(2);
+		super.setType("Rey");
+	}
+}
