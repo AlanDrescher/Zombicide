5e45de4490145f4728cdadaaa9ce144b69dc2d5f
Alan Drescher <alandrescher10@gmail.com>
2024-03-13 08:27:45 +0100

    tienda

--- src/zombicide/Tienda.java
+++ src/zombicide/Tienda.java
@@ -12,7 +12,15 @@ public class Tienda {
 	
 	public void Menu() {
 		Scanner leer = new Scanner(System.in);
+		System.out.println("Oro: " + gold);
 		System.out.println("¡Bienvenido! \r\n ¡Tengo una selección de buenos productos en venta, extranjero!");
+		
+		if (gold > 0) {
+			
+		} else {
+			System.out.println("Vuelve cuando tengas dinero, no somos una ONG");
+			return;
+		}
 	}
 	
 	public void setGold(int gold) {
